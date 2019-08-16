e5f7fb0b5f70f0b26ed9546fb4a4e88ac7c537e4
eric <eric.kingoo2014@gmail.com>
2019-08-16 14:58:42 +0300

    add arrays of female andd male names

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var CC MM DD YY d;
+var dayNames = ["Sunday" ,"Monday" ,"Tuesday" ,"Wednesday" , "Thursday" ,"Friday" ,"Saturday;
+var maleNames = ["Kwasi" , "Kwadwo" , "Kwabena" , "Kwaku" ,"Yaw" , "Kofi" , "Kwame"];
+var femaleNames = ["Akosua" , "Adwoa" , "Abena" , "Akua" , "Yaa" , "Afua" , "Ama"];
